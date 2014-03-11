023ef8a6b6040b2153d6f69e56f4422cfbf3de1b
Ryuichi Itoi <t.link.full.contact@gmail.com>
2014-03-10 22:10:53 -0700

    added bootstrap architectural changes to app

--- js/app.js
+++ js/app.js
@@ -1,45 +1,123 @@
 /**
  * Created by Soundwave on 2/10/14.
  */
+var mapFunctions = (function(){
 
-function getLocation(){
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(getPos, onError);
+    function getLocation(){
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(getPos, onError);
+        }
+
+     }
+
+    function getPos(position){
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+
+        $('#geolocation').text("{\"Latitude\":"+ lat + "," + "\"Longitude\":" + long +"}");
+
+        var userLocation = userLatLng(lat, long);
+        var webMap = googleMap(userLocation);
+        createLocationMarker(webMap, userLocation);
     }
 
-    function getPos(position)
-    {
-        googleMap(position.coords.latitude, position.coords.longitude);
-        alert(position.coords.latitude +", "+ position.coords.longitude)
+    function onError(){
+        alert("Browser did not support geolocation");
     }
 
-    function onError()
-    {
-        alert("Brower did not support geolocation");
-    }
- }
+    function googleMap(userLocale){
 
-function googleMap(latitude, longitude){
+        var myOptions = {
+            zoom : 16,
+            center : userLocale,
+            mapTypeId : google.maps.MapTypeId.ROADMAP
+        };
 
-    var userLatLng = new google.maps.LatLng(latitude, longitude);
-
-    var myOptions = {
-        zoom : 16,
-        center : userLatLng,
-        mapTypeId : google.maps.MapTypeId.ROADMAP
+        return new google.maps.Map(document.getElementById("map-canvas"), myOptions);
     }
 
-    var map = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+    function userLatLng(latitude, longitude){
+        return  new google.maps.LatLng(latitude, longitude);
+    }
 
-    google.maps.event.trigger(map, "resize");
+    function createLocationMarker(mapObj, userLocale){
+        new google.maps.Marker({
+            map: mapObj,
+            position: userLocale
+        });
+    }
 
-    new google.maps.Marker({
-        map: map,
-        position: userLatLng
-    });
+    return{
+      getLocation:getLocation
+    }
 
+})();
 
-}
+var yelpFunctions = (function(){
 
+    var yelpApiEndPoint = "http://api.yelp.com/v2/search";
+
+    var auth = {
+        consumerKey: "eijVNVNlhAlvGVh3CkihCQ",
+        consumerSecret: "fYQqJ8HDfCO-_YF5vEq7vTY7wGw",
+        accessToken: "wU97t7G2_hIZ2uyFM_K_U8XRhkDrP4qk",
+        accessTokenSecret: "9I8b13XIH2b6U-QPvWD9QZdv5Uo",
+        serviceProvider: {
+            signatureMethod: "HMAC-SHA1"
+        }
+    };
+
+    var accessor = {
+        consumerSecret: auth.consumerSecret,
+        tokenSecret: auth.accessTokenSecret
+    };
+
+    function searchYelp(keyword, jsonCoord, radius){
+
+        var x = jsonCoord['Latitude'];
+        var y = jsonCoord['Longitude'];
+
+        params = [];
+        params.push(['term', keyword]);
+        params.push(['ll', x + ',' + y]);
+        params.push(['radius_filter', radius]);
+        params.push(['callback', 'cb']);
+        params.push(['oauth_consumer_key', auth.consumerKey]);
+        params.push(['oauth_consumer_secret', auth.consumerSecret]);
+        params.push(['oauth_token', auth.accessToken]);
+        params.push(['oauth_signature_method', 'HMAC-SHA1']);
+
+        var message = {
+            action: yelpApiEndPoint,
+            method: 'GET',
+            parameters: params
+        };
+
+        OAuth.setTimestampAndNonce(message);
+        OAuth.SignatureMethod.sign(message, accessor);
+
+        var parameterMap = OAuth.getParameterMap(message.parameters);
+        parameterMap.oauth_signature = OAuth.percentEncode(parameterMap.oauth_signature)
+        console.log(parameterMap);
+
+        $.ajax({
+            'url': message.action,
+            'data': parameterMap,
+            'cache': true,
+            'dataType': 'jsonp',
+            'jsonpCallback': 'cb',
+            'success': function(data, textStats, XMLHttpRequest) {
+                console.log(data);
+                var output = JSON.stringify(data);
+                alert(output);
+            }
+        });
+    }
+
+    return{
+        searchYelp:searchYelp
+    }
+})();
 
 
--- js/controller.js
+++ js/controller.js
@@ -5,11 +5,35 @@
 var init = (function(){
 
     function onReady(){
-        //$('#map-canvas').hide();
-        //$('#yelp-info').hide();
+        $('#user-input-search').hide();
+        $('#map-canvas').hide();
+        $('#yelp-info').hide();
 
-        getLocation();
-        googleMap();
+        mapFunctions.getLocation();
+
+        $('#search-tab').click(function(){
+            $('#user-input-search').show();
+            $('#map-canvas').hide();
+            $('#yelp-info').hide();
+
+            var coordStr = $('#geolocation').text();
+            var coordJson = $.parseJSON(coordStr);
+            yelpFunctions.searchYelp("strip club",coordJson,40000);
+        });
+
+        $('#map-tab').click(function(){
+            $('#user-input-search').hide();
+            $('#map-canvas').show();
+            $('#yelp-info').hide();
+
+        });
+
+        $('#results-tab').click(function(){
+            $('#user-input-search').hide();
+            $('#map-canvas').hide();
+            $('#yelp-info').show();
+
+        });
     }
 
     return {
